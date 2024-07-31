d07c709494ef7d5e063fea1353cd22a75cc70e4b
VantorreWannes <VantorreWannes@gmail.com>
2024-07-31 15:23:17 +0200

    Add generic Node struct tests.
    Add generic Node struct test in `root.zig`.
    
    [Issue: #X]

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 const testing = std.testing;
-const Node = @import("node.zig").Node;
+const binary_tree = @import("binary_tree.zig");
 const meta = @import("meta.zig");
+const Node = binary_tree.Node(u8);
 
 export fn add(a: i32, b: i32) i32 {
     return a + b;
@@ -18,4 +19,9 @@ test "basic isNumberType functionality" {
     try testing.expect(meta.isNumberType(*u8));
     try testing.expect(meta.isNumberType(*?u8));
     try testing.expect(!meta.isNumberType(void));
+}
+
+test "basic Node init functionality" {
+    const node = Node.init(42);
+    try testing.expect(node.value == 42);
 }
