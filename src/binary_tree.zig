52bb3fcd180afe61dad5801016719c057cb5e9ba
VantorreWannes <VantorreWannes@gmail.com>
2024-07-31 15:20:14 +0200

    Add type guard to generic Node struct.
    - Make sure the Node struct type can only use a
     number as its value.
    
    [Issue: #X]

--- src/binary_tree.zig
+++ src/binary_tree.zig
@@ -1,13 +1,17 @@
 const std = @import("std");
+const meta = @import("meta.zig");
 
 pub fn Node(comptime T: type) type {
+    if (!meta.isNumberType(T)) @compileError("T must be a number type");
     return struct {
         value: T,
         lesser_node: ?*Node(T) = null,
         greater_node: ?*Node(T) = null,
 
         pub fn init(value: T) Node(T) {
-            return Node(T){ .value = value, };
+            return Node(T){
+                .value = value,
+            };
         }
     };
 }
