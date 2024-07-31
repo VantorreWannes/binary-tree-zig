9cff10339771d0a8f317bad1faa5032643eb1291
VantorreWannes <VantorreWannes@gmail.com>
2024-07-31 14:55:27 +0200

    Implement usefull type info function.
    - Implement usefull isNumberType type info function.
    
    [Issue: #X]

---
+++ src/meta.zig
@@ -0,0 +1,8 @@
+pub fn isNumberType(T: type) bool {
+    return switch (@typeInfo(T)) {
+        .Int, .ComptimeInt, .Float, .ComptimeFloat => true,
+        .Optional => |opt| isNumberType(opt.child),
+        .Pointer => |ptr| if (ptr.size == .One) isNumberType(ptr.child) else false,
+        else => false,
+    };
+}
