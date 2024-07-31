24cd25291cc8686ed83f2abe24bc3fe795b2467d
VantorreWannes <VantorreWannes@gmail.com>
2024-07-31 15:06:21 +0200

    Add `meta.isNumberType` test.
    - Add plenty of important `meta.isNumberType` test
     cases.
    
    [Issue: #X]

--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 const testing = std.testing;
+const Node = @import("node.zig").Node;
+const meta = @import("meta.zig");
 
 export fn add(a: i32, b: i32) i32 {
     return a + b;
@@ -7,4 +9,13 @@ export fn add(a: i32, b: i32) i32 {
 
 test "basic add functionality" {
     try testing.expect(add(3, 7) == 10);
+}
+
+test "basic isNumberType functionality" {
+    try testing.expect(meta.isNumberType(u8));
+    try testing.expect(meta.isNumberType(f32));
+    try testing.expect(meta.isNumberType(?u8));
+    try testing.expect(meta.isNumberType(*u8));
+    try testing.expect(meta.isNumberType(*?u8));
+    try testing.expect(!meta.isNumberType(void));
 }
